759362a5f74fcd9b4b4d5ddd11843d1928d986e4
RustamHadoiev24 <gadoeva155@gmail.com>
2026-06-01 20:39:27 +0300

    Fix movie update logiс

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -21,6 +21,24 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    actors = serializers.PrimaryKeyRelatedField(many=True, queryset=Actor.objects.all())
+    genres = serializers.PrimaryKeyRelatedField(many=True, queryset=Genre.objects.all())
+
     class Meta:
         model = Movie
         fields = ("id", "title", "description", "duration", "actors", "genres")
+
+    def update(self, instance, validated_data):
+        actors = validated_data.pop("actors", None)
+        genres = validated_data.pop("genres", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if actors is not None:
+            instance.actors.set(actors)
+        if genres is not None:
+            instance.genres.set(genres)
+
+        return instance
